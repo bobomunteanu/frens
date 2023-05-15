75445937b9b183f20cb3a0acf8d505f3e79aa254
bobomunteanu <bogdan.munteanu@power.ro>
2023-05-15 23:50:56 +0300

    phone optimisation final

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,42 @@ import NFTLeaderboard from "./components/NFTLeaderboard";
 window.Buffer = buffer.Buffer;
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
+    const [showSidebar, setShowSidebar] = useState(false);
 
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
 
-  return (
-    <div>
-      <button
-        className="toggle-button"
-        onClick={toggleSidebar}
-        style={{
-          marginLeft: "8%",
-          marginTop: "-12%",
-          position: "absolute",
-        }}
-      >
-        <img
-          src="https://icon-library.com/images/hamburger-menu-icon-png/hamburger-menu-icon-png-9.jpg"
-          width="20px"
-        />
-      </button>
-      <div className="main">
-        {showSidebar && <Navbar />}
-        <div style={{ marginLeft: "15vw" }}>
-          <Leaderboard></Leaderboard>
+    return (
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <h1 style={{ marginLeft: "90%" }}>$FRENS</h1>
+                    </td>
+                    <td>
+                        <button
+                            className="toggle-button"
+                            onClick={toggleSidebar}
+                            style={{ marginLeft: "12vw" }}
+                        >
+                            <img
+                                src="https://icon-library.com/images/hamburger-menu-icon-png/hamburger-menu-icon-png-9.jpg"
+                                width="20px"
+                            />
+                        </button>
+                    </td>
+                </tr>
+            </table>
+
+            <div className="main">
+                {showSidebar && <Navbar />}
+                <div style={{ marginLeft: "15vw" }}>
+                    <Leaderboard></Leaderboard>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,72 +2,72 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Leaderboard = () => {
-  const [leaderboardData, setLeaderboardData] = useState([]);
+    const [leaderboardData, setLeaderboardData] = useState([]);
 
-  useEffect(() => {
-    const fetchLeaderboardData = async () => {
-      try {
-        const leaderboardResponse = await axios.get(
-          "https://corsproxy.io/?https://burnfrens-default-rtdb.europe-west1.firebasedatabase.app/users.json"
-        );
+    useEffect(() => {
+        const fetchLeaderboardData = async () => {
+            try {
+                const leaderboardResponse = await axios.get(
+                    "https://corsproxy.io/?https://burnfrens-default-rtdb.europe-west1.firebasedatabase.app/users.json"
+                );
 
-        if (leaderboardResponse.data) {
-          const leaderboard = Object.entries(leaderboardResponse.data).map(
-            ([publicKey, { amount }]) => ({
-              publicKey,
-              amount: amount - 1,
-            })
-          );
-          leaderboard.sort((a, b) => b.amount - a.amount); // Sort in descending order
-          setLeaderboardData(leaderboard.slice(0, 20)); // Get the first 10 accounts
-        }
-      } catch (error) {
-        console.error("Error fetching leaderboard data:", error);
-      }
-    };
+                if (leaderboardResponse.data) {
+                    const leaderboard = Object.entries(
+                        leaderboardResponse.data
+                    ).map(([publicKey, { amount }]) => ({
+                        publicKey,
+                        amount: amount - 1,
+                    }));
+                    leaderboard.sort((a, b) => b.amount - a.amount); // Sort in descending order
+                    setLeaderboardData(leaderboard.slice(0, 20)); // Get the first 10 accounts
+                }
+            } catch (error) {
+                console.error("Error fetching leaderboard data:", error);
+            }
+        };
 
-    fetchLeaderboardData();
+        fetchLeaderboardData();
 
-    // Fetch leaderboard data every 5 seconds
-    const interval = setInterval(fetchLeaderboardData, 5000);
+        // Fetch leaderboard data every 5 seconds
+        const interval = setInterval(fetchLeaderboardData, 5000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  return (
-    <div style={{ marginLeft: "-15%" }}>
-      <h1 style={{ marginLeft: "-15%" }}>$FRENS</h1>
-      <h2 style={{ marginLeft: "-15%" }}>Leaderboard</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Public Key</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {leaderboardData.map(({ publicKey, amount }, index) => (
-            <tr key={publicKey}>
-              <td>#{index + 1}</td>
-              <td>
-                {publicKey.slice(0, 4)}. . .{publicKey.slice(-4)}
-              </td>
-              <td style={{ color: "darkorange" }}>
-                {amount}
-                <img
-                  src="https://raw.githubusercontent.com/bobomunteanu/frens/main/src/assets/tkfrens.png"
-                  width={"20vh"}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div style={{ marginLeft: "-15%" }}>
+            <h2 style={{ marginLeft: "-15%" }}>Leaderboard</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Public Key</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leaderboardData.map(({ publicKey, amount }, index) => (
+                        <tr key={publicKey}>
+                            <td>#{index + 1}</td>
+                            <td>
+                                {publicKey.slice(0, 4)}. . .
+                                {publicKey.slice(-4)}
+                            </td>
+                            <td style={{ color: "darkorange" }}>
+                                {amount}
+                                <img
+                                    src="https://raw.githubusercontent.com/bobomunteanu/frens/main/src/assets/tkfrens.png"
+                                    width={"20vh"}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default Leaderboard;
